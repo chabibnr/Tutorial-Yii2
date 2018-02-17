d9e4d019fa764004cd94f6fb7482513d8d4fb889
Chabibnr <chabibnurozak@gmail.com>
2018-02-17 14:37:44 +0700

    Membuat Read

--- controllers/PhoneBookController.php
+++ controllers/PhoneBookController.php
@@ -7,12 +7,21 @@ use yii\web\Controller;
 
 class PhoneBookController extends Controller {
 
+    public function actionIndex()
+    {
+        return $this->render('index', [
+            'dataProvider' => new \yii\data\ActiveDataProvider([
+                'query' => \app\models\PhoneBook::find(),
+            ]),
+        ]);
+    }
+
     public function actionCreate(){
         $model = new \app\models\PhoneBook();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            echo "Sukses";
-            return;
+
+            return $this->redirect(['index']);
         }
 
         return $this->render('create', [
